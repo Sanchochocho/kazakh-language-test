3b900fecad1ad215c2ebcaf913d9ffee97cbfc94
Ilya Vedyanin <yzbekalesha@dmail.com>
2025-10-04 20:34:41 +0500

    работа закончена

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 import { Link } from "react-router";
 import './Header.css'
@@ -7,13 +8,13 @@ const Header = () => {
         <>
             <header className="header">
                 <div className="container header-inner">
-                    <h1 className="logo">Бағдарламалау тарихы</h1>
+                    <h1 className="logo">Багдарламалау тарихы</h1>
                     <nav>
                         <ul className="nav-list">
                             <Link to={"/"} className="link_main">Негізгі</Link>
                             <Link to={"/test"} className='link_test'>Сынақ</Link>
                             <Link to={"/test_history"} className='link_result'>Нәтижелер</Link>
-                            <button onClick={() => setContactOpen(true)}>Байланыс</button>
+                            <Link onClick={() => setContactOpen(true)}>Байланыс</Link>
                         </ul>
                     </nav>
                 </div>
